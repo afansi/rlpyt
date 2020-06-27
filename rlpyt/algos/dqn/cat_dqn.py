519aa8e3a55396a3612815d0000004f9d25a7355
Your Name <arsene.fansi@gmail.com>
2020-06-27 12:23:14 -0400

    try fix bug cat_dqn.py

--- rlpyt/algos/dqn/cat_dqn.py
+++ rlpyt/algos/dqn/cat_dqn.py
@@ -4,6 +4,7 @@ import torch
 from rlpyt.algos.dqn.dqn import DQN
 from rlpyt.utils.tensor import select_at_indexes, valid_mean
 from rlpyt.algos.utils import valid_from_done
+from rlpyt.utils.buffer import buffer_to
 
 
 EPS = 1e-6  # (NaN-guard)
